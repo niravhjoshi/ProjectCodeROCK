8f466de5c0e8f15f2a9695cf906dcb28ec040a26
Nirav Joshi <nirajosh@in.ibm.com>
2017-04-06 05:34:23 +0530

    New code for iterate using errordict

--- DBFetchCode.py
+++ DBFetchCode.py
@@ -1,12 +1,12 @@
 import MySQLdb
 
-conn = MySQLdb.connect(user="user", passwd="password", db="dbname")
+conn = MySQLdb.connect(user="root", passwd="gravitant", db="centuriondb")
 cur = conn.cursor()
 
-cur.execute("SELECT id, name FROM students")
+cur.execute("SELECT Error_name FROM ErrorsDict_errorsdict")
 row = cur.fetchone()
 while row is not None:
-    print row[0], row[1]
+    print row[0]
     row = cur.fetchone()
 
 cur.close()
